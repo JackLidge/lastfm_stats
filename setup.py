8ef5bdbff619259e37b7bce528c581e0758289e3
JackLidge <jrglmusic@gmail.com>
2020-12-09 21:21:02 +0000

    Updating setup.py to be compliant

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: MIT',
+        'License :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
